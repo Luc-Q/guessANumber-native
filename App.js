7b014438b6423887d75d6df500271bfc3b23940c
Lu <jetqin_2008@hotmail.com>
2022-03-15 22:05:21 +1100

    using style sheet

--- App.js
+++ App.js
@@ -3,12 +3,9 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
     return (
-        <View style={{ padding: 50 }}>
-            <View>
-                <TextInput
-                    placeholder="Course Goal"
-                    style={{ borderBottomColor: "black", borderBottomWidth: 1 }}
-                />
+        <View style={styles.screen}>
+            <View style={styles.inputContaioner}>
+                <TextInput placeholder="Course Goal" style={styles.input} />
                 <Button title="ADD" />
             </View>
             <Text>Open up App.js to start working on your app!</Text>
@@ -16,4 +13,18 @@ export default function App() {
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    screen: {
+        padding: 50,
+    },
+    inputContaioner: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    input: {
+        width: 200,
+        borderColor: "black",
+        borderWidth: 1,
+    },
+});
