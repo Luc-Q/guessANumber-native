0ac2c5baed5938d1c8b90a89f06003973274e3dd
Lu <jetqin_2008@hotmail.com>
2022-03-17 21:31:28 +1100

    some styles

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Button, FlatList } from "react-native";
 import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
     return (
         <View style={styles.screen}>
             <Header title="Guess a number" />
+            <StartGameScreen />
         </View>
     );
 }
