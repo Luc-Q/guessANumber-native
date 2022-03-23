526cbed7a84165a29a157c95acccb35819c0504a
Lu <jetqin_2008@hotmail.com>
2022-03-23 21:14:45 +1100

    add icons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -14,6 +14,7 @@ import Input from "../components/Input";
 import NumberContainer from "../components/NumberContainer";
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
+import MainButton from "../components/MainButton";
 
 const StartGameScreen = (props) => {
     const [enteredValue, setEnteredValue] = useState("");
@@ -54,10 +55,9 @@ const StartGameScreen = (props) => {
             <Card style={styles.summaryContainer}>
                 <BodyText>You selected</BodyText>
                 <NumberContainer>{selectedNumber}</NumberContainer>
-                <Button
-                    title="start game"
-                    onPress={() => props.onStartGame(selectedNumber)}
-                />
+                <MainButton onPress={() => props.onStartGame(selectedNumber)}>
+                    Start Game
+                </MainButton>
             </Card>
         );
     }
